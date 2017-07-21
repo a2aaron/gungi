79d60758b3af7929965d51efc09146a2a9569ea2
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-07-21 11:10:46 -0400

    Move most of the code out of main.rs and into their own seperate files [And More]
    main.rs was getting too big, so I split most of the stuff into tests.rs and pieces.rs.
    
    Other things:
    
    1. Temporarily added `#![allow(dead_code)]` to the entire project because I want rust to shut up about dead code for now (I know it's dead! I haven't written the rest of the things yet!)
    
    2. Fixed the tests to use `static` rather than `const` because `const` does inlining and doesn't guarrentee that two variables will live in different memory locations (which is something that is important for us)
    
    3. Use ptr::eq instead of the weird casting stuff to a constant pointer because it's clearer and isn't as tricky or clever.
    
    4. Add `PieceCombinations`. This is a convience enum that will be used to make Piece struct eventually.
    
    5. Added a bit of documentation.

---
+++ src/pieces.rs
@@ -0,0 +1,169 @@
+/// A tower consists of zero to three pieces. Towers may contain pieces from 
+/// both players. Only the top piece on a tower can move.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Tower<'a> {
+    pub bottom: Option<Piece<'a>>,
+    pub mid: Option<Piece<'a>>,
+    pub top: Option<Piece<'a>>,
+}
+
+/// A tower is valid as long as no two pieces from the same player
+/// of the same type are in it
+///    For example, (Your) Pawn, (Your) Gold, (Your) Gold is disallowed
+///    but (Your) Pawn, (Your) Gold, (Enemy) Gold is fine
+pub fn is_valid(tower: Tower) -> bool {
+    match (tower.bottom, tower.mid, tower.top) {
+        // Empty towers are obviously fine!
+        (None, None, None) => true,
+        // Cases where the tower clearly isn't a tower (ie: bottom pieces missing)
+        (None, _, _) => false,
+        (Some(_), None, Some(_)) => false,
+        // Towers of just one piece can never have two pieces of the same type
+        (Some(_), None, None) => true,
+        (Some(bottom), Some(middle), None) => {
+        // Towers of two mustn't have the pieces be the same type and from same player
+            return !same_type_and_player(bottom, middle)
+        }
+        // Same idea for towers of three but it applies to all piece combinations
+        (Some(bottom), Some(middle), Some(top)) => {
+            return !(same_type_and_player(bottom, middle) ||
+                   same_type_and_player(bottom, top)    || 
+                   same_type_and_player(middle, top))
+        }
+    }
+}
+
+/// Returns true if both pieces have the same PieceType and belong to the same player.
+pub fn same_type_and_player(piece_1: Piece, piece_2: Piece) -> bool {
+    // Compare that the *pointers* are equal, NOT the contents of the pointers
+    // This ensures that the pieces definitely belong to the same player and not just
+    // different players that happen to look like each other.
+    use std::ptr::eq;
+    let same_player = eq(piece_1.belongs_to, piece_2.belongs_to);
+    let same_type = piece_1.current_type() == piece_2.current_type();
+    return same_player && same_type
+           
+}
+
+/// A piece has two sides, called "Front" and "Back." Pieces initially
+/// start out as their Front side but will flip to Back if they are captured.
+/// The only piece that does not have this is the Commander, which is similar
+/// to the king in chess.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Piece<'a> {
+    // This should be either front_side or back_side.
+    // May change when piece is captured
+    pub current_side: SideType,
+    pub front_side: PieceType,
+    pub back_side: PieceType,
+    // We use a pointer here because the player owns the piece, not 
+    // the other way around.
+    pub belongs_to: &'a Player,
+}
+
+impl<'a> Piece<'a> {
+    pub fn current_type(&self) -> PieceType {
+        use SideType::*;
+        match self.current_side {
+            Front => self.front_side,
+            Back => self.back_side
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Player {
+    // TODO
+}
+
+impl Player {
+    // Stub for the Player struct
+    pub fn new_blank() -> Player {
+        return Player {
+
+        }
+    }
+}
+
+/// Return what side a PieceType is.
+/// For example, `get_sidet_type(Spy)` returns `Front`
+pub fn get_side_type(piece_type: PieceType) -> SideType {
+    use PieceType::*;
+    use SideType::*;
+    match piece_type {
+        Commander => Front,
+        Captain => Front,
+        Samurai => Front,
+        Spy => Front,
+        Catapult => Front,
+        Fortress => Front,
+        HiddenDragon => Front,
+        Prodigy => Front,
+        Bow => Front,
+        Pawn => Front,
+        Pistol => Back,
+        Pike => Back,
+        Clandestinite => Back,
+        Lance => Back,
+        DragonKing => Back,
+        Phoenix => Back,
+        Arrow => Back,
+        Bronze => Back,
+        Silver => Back,
+        Gold => Back,
+    }
+}
+
+/// A Piece has both a Front and a Back side, which determines what
+/// PieceType it wil lact as.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum SideType {
+    Front, Back
+}
+
+/// All of the avaliable Gungi pieces 
+/// This enum is useful as a shorthand for creating new Piece structs
+/// these variants listed are the only combinations of Front and Back
+/// pieces found.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum PieceCombinations {
+    Commander,
+    CaptainPistol,
+    SamuraiPike,
+    SpyCladestinite,
+    CatapultLance,
+    FortressLance,
+    HiddenDragonDragonKing,
+    ProdigyPhoenix,
+    BowArrow,
+    PawnBronze,
+    PawnSilver,
+    PawnGold,
+}
+
+/// All the individal piece types.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum PieceType {
+    // Front Pieces
+    Commander, // Command has no back piece!
+    Captain,
+    Samurai,
+    Spy,
+    Catapult,
+    Fortress,
+    HiddenDragon,
+    Prodigy,
+    Bow,
+    Pawn,
+    // Back Pieces
+    Pistol,
+    Pike,
+    Clandestinite,
+    Lance,
+    DragonKing,
+    Phoenix,
+    Arrow,
+    Bronze,
+    Silver,
+    Gold,
+}
---
+++ src/tests.rs
@@ -0,0 +1,147 @@
+#[cfg(test)]
+mod tests {
+    use pieces::*;
+    // Yes, use static here instead of const because we do want 
+    // these pieces to refer to entirely different objects
+    // (Player 1 should not have the same memory location as Player 2!)
+    static PLAYER_1: Player = Player {};
+    static PLAYER_2: Player = Player {};
+
+    // Some example pieces for testing
+    // Front Pawn, Back Gold, Player 1
+    static PIECE_1A: Piece = Piece {
+                    current_side: SideType::Front,
+                    front_side: PieceType::Pawn,
+                    back_side: PieceType::Gold,
+                    belongs_to: &PLAYER_1
+                    };
+    // Front Bow, Back Silver, Player 1
+    static PIECE_1B: Piece = Piece {
+                    current_side: SideType::Front,
+                    front_side: PieceType::Bow,
+                    back_side: PieceType::Silver,
+                    belongs_to: &PLAYER_1
+                    };
+
+    // Front Pawn, Back Bronze, Player 2
+    static PIECE_2A: Piece = Piece {
+                    current_side: SideType::Front,
+                    front_side: PieceType::Pawn,
+                    back_side: PieceType::Bronze,
+                    belongs_to: &PLAYER_2
+                    };
+
+    // Front Bow, Back Arrow, Player 2
+    static PIECE_2B: Piece = Piece {
+                    current_side: SideType::Front,
+                    front_side: PieceType::Bow,
+                    back_side: PieceType::Arrow,
+                    belongs_to: &PLAYER_2
+                    };
+
+    #[test]
+    fn test_valid_towers() {
+        let empty_tower = Tower {bottom: None, mid: None, top: None};
+        assert!(is_valid(empty_tower));
+
+
+        let single_tower = Tower {bottom: Some(PIECE_1A), mid: None, top: None};
+        assert!(is_valid(empty_tower));
+
+
+        let double_tower = Tower {bottom: Some(PIECE_1A), mid: Some(PIECE_1B), top: None};
+        assert!(is_valid(double_tower));
+
+        let double_same_type_diff_player_tower = Tower {bottom: Some(PIECE_1A), mid: Some(PIECE_2A), top: None};
+        assert!(is_valid(double_same_type_diff_player_tower));
+
+        let triple_tower = Tower {bottom: Some(PIECE_1A), mid: Some(PIECE_1B), top: Some(PIECE_2A)};
+        assert!(is_valid(triple_tower));
+    }
+
+    #[test]
+    fn test_invalid_towers() {
+        // Towers can't have any holes in them
+        let bad_tower_mid = Tower {bottom: None, mid: Some(PIECE_1A), top: None};
+        assert!(!is_valid(bad_tower_mid));
+
+        let bad_tower_top = Tower {bottom: None, mid: None, top: Some(PIECE_1A)};
+        assert!(!is_valid(bad_tower_top));
+
+        let bad_tower_mid_top = Tower {bottom: None, mid: Some(PIECE_1A), top: Some(PIECE_1A)};
+        assert!(!is_valid(bad_tower_mid_top));
+
+        let bad_tower_bot_top = Tower {bottom: Some(PIECE_1A), mid: None, top: Some(PIECE_1A)};
+        assert!(!is_valid(bad_tower_bot_top));
+    }
+
+    #[test]
+    fn test_same_type_and_player() {
+        // Same piece types but one is on the back (true)
+        let piece_1 = Piece {
+                        current_side: SideType::Front,
+                        front_side: PieceType::Pawn,
+                        back_side: PieceType::Gold,
+                        belongs_to: &PLAYER_1
+                        };
+
+        let piece_2 = Piece {
+                        current_side: SideType::Back,
+                        front_side: PieceType::Silver,
+                        back_side: PieceType::Pawn,
+                        belongs_to: &PLAYER_1
+                        };
+        assert!(same_type_and_player(piece_1, piece_2), "Expected the types to be the same even though the current side is different.");
+
+        // Same pieces but different current sides (false)
+        let piece_3 = Piece {
+                        current_side: SideType::Front,
+                        front_side: PieceType::Pawn,
+                        back_side: PieceType::Gold,
+                        belongs_to: &PLAYER_1
+                        };
+
+        let piece_4 = Piece {
+                        current_side: SideType::Back,
+                        front_side: PieceType::Pawn,
+                        back_side: PieceType::Gold,
+                        belongs_to: &PLAYER_1
+                        };
+        assert!(!same_type_and_player(piece_3, piece_4), "Expected the types to be different even though the sides are the same");
+
+        // Same piece types but different players (false)
+        let piece_5 = Piece {
+                        current_side: SideType::Front,
+                        front_side: PieceType::Pawn,
+                        back_side: PieceType::Gold,
+                        belongs_to: &PLAYER_1
+                        };
+
+        let piece_6 = Piece {
+                        current_side: SideType::Front,
+                        front_side: PieceType::Pawn,
+                        back_side: PieceType::Gold,
+                        belongs_to: &PLAYER_2
+                        };
+        assert!(!same_type_and_player(piece_5, piece_6), "Expected the players to be different even though the sides and type are the same");
+    }
+
+    #[test]
+    fn test_current_type() {
+        let front_piece = Piece {
+                        current_side: SideType::Front,
+                        front_side: PieceType::Pawn,
+                        back_side: PieceType::Gold,
+                        belongs_to: &Player::new_blank()
+                        };
+        assert_eq!(PieceType::Pawn, front_piece.current_type());
+
+        let back_piece = Piece {
+                        current_side: SideType::Back,
+                        front_side: PieceType::Pawn,
+                        back_side: PieceType::Gold,
+                        belongs_to: &Player::new_blank()
+                        };
+        assert_eq!(PieceType::Gold, back_piece.current_type());
+    }
+}
