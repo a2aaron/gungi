6abd65b25153436220988fd87463cd68789aabe0
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-07-20 20:18:08 -0400

    Add basic enum for PieceTypes
    There are 20 unique pieces types in the Gungi board game. A piece consists of a black and white chip. On one side is a "Front" type piece and on the back are the "Back" type pieces. Every Front Type piece (except for Commander) has a Back Type piece.
    
    Gungi Rules: https://mmmmalo.tumblr.com/post/74510568781/rules-of-gungi

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+// GUNGI RULES
+//https://mmmmalo.tumblr.com/post/74510568781/rules-of-gungi
 fn main() {
     println!("Hello World!");
+}
+
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum PieceType {
+    // Front Pieces
+    Commander, // Command has no back piece!
+    Captain,
+    Samurai,
+    Spy,
+    Catapult,
+    Fortress,
+    HiddenDragon,
+    Prodigy,
+    Bow,
+    Pawn,
+    // Back Pieces
+    Pistol,
+    Pike,
+    Clandestinite,
+    Lance,
+    DragonKing,
+    Phoenix,
+    Arrow,
+    Bronze,
+    Silver,
+    Gold,
 }
