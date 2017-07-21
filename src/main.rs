6063f0919abafd47a5d74d31190b8cb29fa8f6ee
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-07-20 20:31:06 -0400

    Add skeleton structs for Piece and Player.
    The Piece struct stores any important information about the current piece, such as the player it currently belongs to, the side it is currently on, and maybe more in the future.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,23 @@ fn main() {
     println!("Hello World!");
 }
 
+/// A piece has two sides, called "Front" and "Back." Pieces initially
+/// start out as their Front side but will flip to Back if they are captured.
+/// The only piece that does not have this is the Commander, which is similar
+/// to the king in chess.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Piece<'a> {
+    // This should be either front_side or back_side. May change when piece is captured
+    pub current_side: PieceType,
+    front_side: PieceType,
+    back_side: PieceType,
+    pub belongs_to: &'a Player,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Player {
+    // TODO
+}
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum PieceType {
